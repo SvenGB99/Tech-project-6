74cd335a8487d634eded8f9268e70e6103a9231c
SvenGB99 <76749289+SvenGB99@users.noreply.github.com>
2021-03-29 14:16:47 -0700

    setting up the starter java script

---
+++ apps.js
@@ -0,0 +1,12 @@
+const qwerty = document.querySelector ('qwerty');
+const phrase = document.querySelector ('phrase');
+const button = document.querySelector ('btn__reset');
+
+let missed = 0;
+
+const phrases = ('Cards Against Humanity',
+                    'Setlers of Catan',
+                    'Money for Nothing',
+                    'Playstation three',
+                    'treehouse of horrors');
+

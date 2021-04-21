c217330f547a93bfda5fe352315485ae59f5fd9c
SvenGB99 <76749289+SvenGB99@users.noreply.github.com>
2021-04-21 14:10:24 -0700

    submission
    final destination

--- app.js
+++ app.js
@@ -1,116 +1,115 @@
-
-// Variables
-const keyBoard = document.getElementById('qwerty');
-const phrase = document.getElementById('phrase');
-const startGame = document.querySelector('.btn__reset');
-const listUl = document.querySelector('ul');
-const hearts = document.querySelector('.tries');
-const letter = document.querySelector('letters');
-
-
-// missed start at 0 always
+//Variables as per the guide
+const keyboard = document.querySelector('#qwerty');   
+const phrase = document.getElementById=('phrase');  
+const startButton = document.querySelector('.btn__reset');
 let missed = 0;
 
 
-// phrases
-const list = [
-    'I had the time of my life',
-    'bouncing is what tiggers do best',
-    'if i had a million dollars',
-    'lets get dangerous',
-    'fuzzy wazzy was a bear',
-    'running down a dream'
+// Phrase array
+let phrases = [
+  'I had the time of my life',
+  'bouncing is what tiggers do best',
+  'if i had a million dollars',
+  'lets get dangerous',
+  'fuzzy wazzy was a bear',
+  'running down a dream'
 ];
 
-
-// Variables
-
 // event listener for click of start button
-startGame.addEventListener('click', () => {
-    overlay.style.display = 'none'
-    console.log('this made it through');
-});
+const homepage = document.querySelector('#overlay');
+startButton.addEventListener('click', ()=>{
+    homepage.style.display = 'none';
+  });
 
-// get random phrase from arry via random number selection and split the em up
-function getRandomPhrase(arr) {
-    let randomP = arr[Math.floor(Math.random() * arr.length)];
-    return randomP;
-}
-
-let newP = getRandomPhrase(list);
+/* Use the variable to select an index inside of the array*/
+function getRandomPhraseAsArray(arr) {
+    let randomArr = arr[Math.floor(Math.random() * phrases.length)];   
+    return splitArray = randomArr.split([,]);
+}      
 
 // creat addPhraseToDisplay function
-
-function addPhraseToDisplay(arr) {
-    for( let i = 0; i < newP.length; i++) {
-        const li = document.createElement('li');
-        listUl.appendChild(li);
-        li.textContent = arr[i];
-
-        if (arr[i] === ' ') {
-            li.classList.add("space");
-        } else {
-            li.classList.add('letter');
-        }
+const phraseList = document.querySelector('#phrase ul');
+function addPhraseToDisplay (arr)  {
+  //create list li item loop
+  for(let i = 0; i < arr.length; i++) {     
+    const li = document.createElement('li');
+    phraseList.appendChild(li);
+    li.textContent = arr[i];
+    // insert an if statement here to check if arr[i] is a space, if it is give it the class of "space"
+    if (arr[i] === ' '){
+      li.classList.add("space");
+    } else {
+      li.classList.add("letter");
     }
+  }
 }
 
-const randomPhrase = getRandomPhrase(list);
 
-addPhraseToDisplay(randomPhrase)
+//To use the function, you’ll get the value returned by the getRandomPhraseAsArray, save it to a variable 
+const RandomPhrase = getRandomPhraseAsArray(phrases); 
 
-// check if a letter is in the phrase
-const checkletter = (button) => {
+//and pass it to addPhraseToDisplay as an argument
+addPhraseToDisplay(RandomPhrase);
+
+
+//check letter function
+const letters = document.querySelectorAll('.letter');
+const checkLetter = (button) => {
     let matched = null;
-    for( i = 0; i < letter.length; i++ ) {
-        if (keyBoard === letters[i].textContent.toUppeCase()) {
-            letter[i].classList.add('show');
-            matched = true;
-        }
+    for (i=0; i < letters.length; i++) {
+            if (button === letters[i].textContent.toLowerCase()) {
+        letters[i].classList.add('show');
+        matched = true;
+      }
     }
-    return matched;
-}
+        return matched;
+  }
 
-// adding event listener for the keyboard
 
-keyBoard.addEventListener('click', e => {
-    if (e.target.tagName === "button") {
+//Add an event listener to the keyboard
+const liveHeart = document.querySelectorAll('.tries img');
+keyboard.addEventListener('click', e => {
+    if (e.target.tagName === "BUTTON") {
         e.target.className = 'chosen';
         e.target.disabled = true;
 
-        const match = checkLetter(e.target.textContent.toUppeCase());
+        const match = checkLetter(e.target.textContent.toLowerCase());
+
         if (match === null) {
-            hearts[missed].src = 'images/lostHeart.png'; // if you gess a letter wrong this will change the heart to a light blue.
+            // code to change heart icon from liveHeart.png to lostHeart.png would go here
+            liveHeart[missed].src = 'images/lostHeart.png';
             missed++;
         }
+        // checkWin() function call would go here, the function is created as below
         checkWin();
     }
-    // this will allow you to rest the game
-    reset();
+    reset();  //reset game after win or lose, the function is at the end
 });
 
-// winner winner chicken dinner
 
+//create checkWin function
 function checkWin () {
-    const show = document.querySelectorAll('.show');
-    let message = document.querySelector('.title');
-    if (letters.length === show.length) {
-        homepage.className = "win";
-        message.textContent = `you are the winner!!`;
-        homepage.style.display = 'flex';
-    } else {
-        homepage.className = "lose";
-        message.textContent = `sorry try again...`;
-        homepage.style.display = 'flex';
-    }
+  const show = document.querySelectorAll('.show');
+  let message = document.querySelector('.title')
+  if (letters.length === show.length) {
+      homepage.className = "win"; 
+      message.textContent = `Congrats you guessed the puzzle!`;
+      homepage.style.display = "flex";
+  } else if (missed > 4) {
+      homepage.className = "lose"; 
+      message.textContent = `Try again next time`;
+      homepage.style.display = "flex";
+      }   
 }
 
-// lets try that again with a reset function
+
+// create reset function
 
 function reset () {
-    startGame.textContent = 'new puzzle';
-    startGame.addEventListener('click', ()=> {
-        location.reload();
-        startGame.styel.transistion = '5s';
-    });
-}
+  startButton.textContent = 'play again';
+  startButton.addEventListener('click', ()=> {
+      location.reload();
+      startButton.style.transition = '5s';
+  });
+
+}   
